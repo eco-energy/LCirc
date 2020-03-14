821a98d3fad8e6dd5406dc4c7cf2cfc8d6c6f384
Faez Shakil <faez.shakil@gmail.com>
2020-03-14 13:30:00 +0500

    serial composition now implemented!!!

--- src/ConCat/LCirc.hs
+++ src/ConCat/LCirc.hs
@@ -20,6 +20,12 @@ import GHC.Generics (Generic)
 import Data.Bifunctor
 import qualified ConCat.Additive as A
 import Data.Finite
+import GHC.TypeLits (KnownNat(..))
+import qualified Data.Set as Set
+import Data.Set (Set(..))
+import qualified Data.Map.Strict as Map
+import Data.Map.Strict (Map(..))
+
 -- A category where the morphisms are circuits made of wires with circuit elemtns on them
 
 
@@ -41,20 +47,58 @@ import Data.Finite
 type R = Double
 
 newtype Edge l v = Edge (Pair v, l) deriving (Show, Generic)
+
 mkEdge :: v -> v -> l -> Edge l v
 mkEdge s t l = Edge (s :# t, l)
 
+replaceMatching :: (Eq v) => Maybe (v -> v) -> Maybe (v -> v) -> Edge l v ->  Edge l v
+replaceMatching (Just f) (Just g) e@(Edge (s :# t, l)) = (Edge (f s :# g t, l))
+replaceMatching (Just f) Nothing e@(Edge (s :# t, l)) = (Edge (f s :# t, l))
+replaceMatching Nothing (Just g) e@(Edge (s :# t, l)) = (Edge (s :# g t, l))
+replaceMatching Nothing Nothing e@(Edge (s :# t, l)) = e
+
+
+replaceEdges :: (Ord v, Ord l) => Edges l v -> Edges l v -> Map v (v -> v) -> Edges l v
+replaceEdges e1 e2 e12 = Set.union e1 e2'
+  where
+    e2' = Set.map re e2
+    re e = replaceMatching (Map.lookup (src e) e12) (Map.lookup (tgt e) e12) e
+
+getFn :: (Ord v) => Map v (v -> v) -> v -> Maybe (v -> v)
+getFn fnMap n = Map.lookup n fnMap
+
 instance (Eq l, Eq v) => Eq (Edge l v) where
-  (Edge (a :# b, l)) == (Edge (a' :# b', l')) = a == a' && b == b' && l == l'
+  (Edge (a :# b, l)) == (Edge (a' :# b', l')) = (a == a' && b == b' || a == b' && b == a') && l == l'
 
-type Edges l v = [Edge l v]
+instance (Ord l, Ord v) => Ord (Edge l v) where
+  (Edge (a :# b, l)) <= (Edge (a' :# b', l')) = (a, b, l) <= (a', b', l')
 
-type Nodes v = [v]
+type Edges l v = Set (Edge l v)
+
+type Nodes v = Set v
 
 newtype LG l v = LG { runLG :: (Nodes v, Edges l v) } deriving (Eq, Show, Generic)
 
+nodes :: LG l v -> Nodes v
+nodes = fst . runLG
+
+edges :: LG l v -> Edges l v
+edges = snd . runLG
+
 mkLG :: Nodes v -> Edges l v -> LG l v
 mkLG = curry LG
+
+mkNodes :: (Ord v) => [v] -> Nodes v
+mkNodes = Set.fromList
+
+mergeNodes :: (Ord v) => Nodes v -> Nodes v -> Map v (v -> v) -> Nodes v
+mergeNodes n n' chngs = Set.union n n'_
+  where
+    n'_ = foldl (\k nn-> Set.delete nn k) n' (Map.keys chngs)
+
+mkEdges :: (Ord v, Ord l) => [Edge l v] -> Edges l v
+mkEdges = Set.fromList
+
 
 src :: Edge l v -> v
 src (Edge (s :# _, _)) = s
@@ -69,56 +113,20 @@ label (Edge (_, l)) = l
 {---------------------------------------------------------------------------------
     Cospans in a Category
 ---------------------------------------------------------------------------------}
+{--
+newtype Cospan v i o = Cospan
+  { runCospan :: (i -> v, o -> v, [v]) } deriving (Generic)
 
-newtype Cospan k v i o = Cospan { runCospan :: (i `k` v, o `k` v) } deriving (Generic, Show)
+mkCospan :: (i -> v) -> (o -> v) -> [v] -> Cospan v i o
+mkCospan i o vs = Cospan (i, o, vs)
 
-mkCospan :: i `k` v -> o `k` v -> Cospan k v i o
-mkCospan = curry Cospan
-
-instance (Category k, Ok k v) => Category (Cospan k v)
-
-
-data Port a = Input (Int, Int) | Output (Int, Int) deriving (Show)
-
-type Inputs a = [Port (CIdx a)]
-type Outputs a = [Port (CIdx a)]
-
-
---cmpC :: Cospan k v i o -> Cospan k v o o' -> Cospan k v i o'
---cmpC (Cospan (i, o)) (Cospan (i', o')) = 
-
-type CospanC i o = Cospan (->) (Nodes Int) (Inputs i) (Outputs o)
-
-
-
-{---------------------------------------------------------------------------------
-    An LCirc is a Pair of an LGraph over a label-set of Circuit Elements
-    and a Cospan over the category of Finite Sets.
----------------------------------------------------------------------------------}
-
-newtype LCirc l v i o = LCirc (LG l v, CospanC i o) deriving (Generic)
-
-instance (Show l, Show v) => Show (LCirc l v i o) where
-  show (LCirc (lg, Cospan (i, o))) = "LCirc: " <> (show lg)
-
-instance (Eq l, Eq v) => Eq (LCirc l v i o) where
-  (LCirc (lg, Cospan (i, o))) == (LCirc (lg', Cospan (i', o'))) = lg == lg'
-
-mkLC = curry LCirc
-
-
-input :: Cospan k v i o -> i `k` v
-input (Cospan (i, _)) = i
-
-output :: Cospan k v i o -> o `k` v
-output (Cospan (_, o)) = o
-
-unitR :: LG CircEl Int
-unitR = mkLG [1, 2] [mkEdge 1 2 $ Res 0]
-
-data CircEl = Res R | Cap R | Ind R deriving (Eq, Ord, Show, Generic)
-
-
+compCospan :: (Eq v) => Cospan v i o -> Cospan v o o' -> Cospan v i o
+compCospan (Cospan (f, g, vs)) (Cospan (h, k, vs')) = mkCospan (iN . f) (iP . g) vs''
+  where
+    iP = undefined
+    iN = undefined
+    vs'' = identify vs vs'
+--}
 
 {-------------------------
      Operadic Machinery
@@ -133,70 +141,117 @@ type Three = S (S (S Z))
 data CIdx n where
   CS0 :: CIdx Z
   CSCons :: CIdx n -> CIdx (S n)
+---------------------------------}
+
+type PortId = Int
+type NodeId = Int
+
+data Port v a = Port (PortId, v) deriving (Eq, Ord, Show)
+
+unifyPorts :: (Eq v) => Port v a -> Port v a -> (v, (v -> v))
+unifyPorts (Port (p, n)) (Port (p', n')) = (n', (\c -> if c == n' then n else c)) 
+
+unifyComposablePortSets ::  (Ord v) => Inputs v i -> Outputs v i -> Map v (v -> v)
+unifyComposablePortSets is os = Map.fromList $ map (uncurry unifyPorts) $ zip os is 
+
+type Inputs v a = [Port v (CIdx a)]
+type Outputs v a = [Port v (CIdx a)]
 
 
-r3 :: LG CircEl Int
-r3 = mkLG [1, 2] [mkEdge 1 2 $ Res 3] 
+--cmpC :: Cospan k v i o -> Cospan k v o o' -> Cospan k v i o'
+--cmpC (Cospan (i, o)) (Cospan (i', o')) = 
 
-circuitEx :: LG CircEl Int
-circuitEx = mkLG [1, 2, 3, 4] [ mkEdge 1 2 $ Res 2
-                              , mkEdge 1 2 $ Cap 3
-                              , mkEdge 1 3 $ Res 1
-                              , mkEdge 3 4 $ Ind 1
-                              , mkEdge 4 2 $ Res 1
-                              ] 
+newtype CospanC v i o = CospanC (Inputs v i, Outputs v o)
 
-circuitEx' :: LG CircEl Int
-circuitEx' = mkLG [5, 6, 7] [ mkEdge 5 6 $ Res 5
-                            , mkEdge 6 7 $ Res 8
-                            ]
+mkCospanC :: Inputs v i -> Outputs v o -> CospanC v i o
+mkCospanC = curry CospanC
+
+--mkCospanC' :: [Port (CIdx i)] -> [Port (CIdx o)] -> CospanC i o
+--mkCospanC' is os = mkCospanC (Set.fromList is) (Set.fromList os)
 
 
+{---------------------------------------------------------------------------------
+    An LCirc is a Pair of an LGraph over a label-set of Circuit Elements
+    and a Cospan over the category of Finite Sets.
+---------------------------------------------------------------------------------}
 
-{-------------------------
- Operadic Circuit Cospans
---------------------------}
+data CircEl = Res R | Cap R | Ind R deriving (Eq, Ord, Show, Generic)
+
+newtype LCirc l v i o = LCirc { runLCirc :: (LG l v, CospanC v i o) } deriving (Generic)
+
+instance (Show l, Show v) => Show (LCirc l v i o) where
+  show (LCirc (lg, CospanC (i, o))) = "LCirc: " <> (show lg)
+
+instance (Eq l, Eq v) => Eq (LCirc l v i o) where
+  (LCirc (lg, CospanC (i, o))) == (LCirc (lg', CospanC (i', o'))) = lg == lg'
+    && i == i'
+    && o' == o'
+
+mkLC = curry LCirc
+
+input :: CospanC v i o -> Inputs v i
+input (CospanC (i, _)) = i
+
+output :: CospanC v i o -> Outputs v o
+output (CospanC (_, o)) = o
 
 
-intCospan :: [Port (CIdx i)] -> [Port (CIdx o)] -> CospanC i o
-intCospan is os = (mkCospan (\_ -> map getI is) (\_ -> map getO os))
+composeLC :: (Ord v, Ord l) => LCirc l v i o -> LCirc l v o o' -> LCirc l v i o'
+composeLC (LCirc ((LG (n, e)), CospanC (i, o))) (LCirc ((LG (n', e')), CospanC (i', o'))) = mkLC lg'' cspan''
   where
-    getI (Input (_, n)) = n
-    getO (Output (_, n)) = n
+    --unifyComposablePortSets :: Inputs v i -> Outputs i -> Map NodeId (NodeId -> NodeId)
+    replacements = unifyComposablePortSets i' o 
+    lg'' = LG (mergeNodes n n' replacements, replaceEdges e e' replacements)
+    o'' = map (\(Port (pid, nid)) -> case Map.lookup nid replacements of
+                  Just nid' -> Port (pid, nid' nid)
+                  Nothing -> Port (pid, nid)
+              ) o'
+    cspan'' = CospanC (i, o'')
 
-mkInput = curry Input
-mkOutput = curry Output
+mkLG' ns es = mkLG (mkNodes ns) $ mkEdges es 
 
-exCospan :: CospanC One Two
-exCospan = intCospan
+unitR :: LG CircEl NodeId
+unitR = mkLG' [1, 2] [mkEdge 1 2 $ Res 0]
+
+
+r3 :: LG CircEl NodeId
+r3 = mkLG' [1, 2] [mkEdge 1 2 $ Res 3] 
+
+circuitEx :: LG CircEl NodeId
+circuitEx = mkLG' [1, 2, 3, 4] [ mkEdge 1 4 $ Res 2
+                               , mkEdge 1 4 $ Cap 3
+                               , mkEdge 1 2 $ Res 1
+                               , mkEdge 2 3 $ Ind 1
+                               , mkEdge 3 4 $ Res 1
+                               ] 
+
+circuitEx' :: LG CircEl NodeId
+circuitEx' = mkLG' [5, 6, 7] [ mkEdge 5 6 $ Res 5
+                             , mkEdge 6 7 $ Res 8
+                             ]
+mkPort = curry Port
+mkInput = mkPort
+mkOutput = mkPort
+
+exCospan :: CospanC NodeId One Two
+exCospan = mkCospanC
   [(mkInput 1 1)]
-  [ (mkOutput 1 2)
-  , (mkOutput 2 2)] 
+  [ (mkOutput 1 4)
+  , (mkOutput 2 4)] 
 
-exCospan' :: CospanC Two Three 
-exCospan' = intCospan
+exCospan' :: CospanC NodeId Two Three 
+exCospan' = mkCospanC
   [(mkInput 1 5), (mkInput 2 7)]
   [(mkOutput 1 5), (mkOutput 2 7)]
 
-exLC :: LCirc CircEl Int One Two
+exLC :: LCirc CircEl NodeId One Two
 exLC = mkLC circuitEx exCospan
 
-exLC' :: LCirc CircEl Int Two Three
+exLC' :: LCirc CircEl NodeId Two Three
 exLC' = mkLC circuitEx' exCospan'
 
-composeLC :: LCirc l v i o -> LCirc l v o o' -> LCirc l v i o'
-composeLC (LCirc (lg, Cospan (i, o))) (LCirc (lg', Cospan (i', o'))) = mkLC lg'' cspan''
-  where
-    -- For a list of nodes, get all the edges
-    lg'' = compLG o i' lg lg'
-    cspan'' = undefined --compCS cspan cspan'
-
-compLG :: (Outputs i -> Nodes Int) -> (Inputs i -> Nodes Int) -> LG l v -> LG l v -> LG l v
-compLG o i' lg lg' = undefined
 
 
-compCS :: Cospan k v i o -> Cospan k v o o' -> Cospan k v i o'
-compCS (Cospan (i, o)) (Cospan (i', o')) = mkCospan i o'
 
 {----------------------------------------------------------------------------------------
          Category Instances
